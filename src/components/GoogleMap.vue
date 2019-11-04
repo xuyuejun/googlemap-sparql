52df0900aba065b21f39d481daf5a52ffec4a06e
xuyuejun <lwxuyuejun@163.com>
2019-11-04 12:27:54 +0900

    Add GmapMap

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="container">
-        <button @click="generate">Generate and Bound to Lat Lngs</button>
+        <GmapMap
+            :center="{lat:10, lng:10}"
+            :zoom="7"
+            map-type-id="terrain"
+            style="width: 100%; height: 100%"
+        >
+            <GmapMarker
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="true"
+                @click="center=m.position"
+            />
+        </GmapMap>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAKfefiVhHqXWQsYDPJjs5t5Nhj5Qar2AE',
+    libraries: 'places'
+  }
+});
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
