ae601acfcc6e56fd4a9f543dee1699b2faf05c50
xuyuejun <lwxuyuejun@163.com>
2020-01-13 17:23:46 +0900

    change sparql.js

--- src/api/runsparql.js
+++ src/api/runsparql.js
@@ -1,11 +1,32 @@
-import axios from 'axios'
+
+class SPARQLQueryDispatcher {
+    constructor( endpoint ) {
+        this.endpoint = endpoint;
+    }
+
+    query( sparqlQuery ) {
+        const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
+        const headers = { 'Accept': 'application/sparql-results+json' };
+
+        return fetch( fullUrl, { headers } ).then( body => body.json() );
+    }
+}
+
+const endpointUrl = 'https://query.wikidata.org/sparql';
+
+const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
 
 export default {
-    runSparql (query) {
-        return axios({
-            url: 'sparql?query=' + query,
-            method: 'get',
-            baseURL: 'http://ja.dbpedia.org/'
-        })
+    searchAirport (country) {
+        const searchAirport = `SELECT ?airport ?airportLabel ?place_served ?coor ?website ?image WHERE {
+          ?airport wdt:P17 wd:`+ country +`;
+            wdt:P31 wd:Q1248784;
+            wdt:P931 ?place_served;
+            wdt:P625 ?coor;
+            wdt:P856 ?website;
+            wdt:P18 ?image.
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],zh". }
+        }`;
+        return queryDispatcher.query( searchAirport )
     }
 }
